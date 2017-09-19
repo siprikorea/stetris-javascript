25d62ae0f4ee0b01e2d3820d19fe3df5b840712e
Chanbong Park <siprikorea@gmail.com>
2017-09-19 23:17:38 +0900

    Add stapp.cpp stblock.cpp stblock.h

---
+++ stblock.h
@@ -0,0 +1,21 @@
+#define MAX_BLOCK_CNT 7
+#define MAX_BLOCK_X 4
+#define MAX_BLOCK_Y 4
+
+class stblock
+{
+    public:
+        // constructor
+        stblock(int no);
+        // rotate block
+        void rotate();
+        // get block
+        void get_block(bool* block);
+
+    private:
+        // block number
+        int m_no;
+        // blocks
+        bool m_block[MAX_BLOCK_Y][MAX_BLOCK_X];
+};
+
